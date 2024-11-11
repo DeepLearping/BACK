be67867f5b3c339c11b28b6589d662c6c81212e4
juyeon99 <juyeonh@sfu.ca>
2024-11-11 14:12:33 +0900

    first commit

---
+++ src/main/java/com/dlp/back/DlpBackApplication.java
@@ -0,0 +1,13 @@
+package com.dlp.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlpBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dlp/back/DlpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlp.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlpBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
